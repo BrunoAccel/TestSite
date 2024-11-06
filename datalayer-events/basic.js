88fdd59a0c04b623f48a6b923ad56a8b1b0c421f
BrunoAccel <bruno.anselmo@groupm.com>
2024-11-06 12:40:57 +0000

    Add files via upload

---
+++ datalayer-events/basic.js
@@ -0,0 +1,18 @@
+function dl_page_view() {
+dataLayer.push({
+  event: "page_view",
+  page_section: "test section"
+};
+
+function dl_test_event() {
+dataLayer.push({
+  event: "test_event",
+  test_variable: "test value"
+};
+
+function dl_conversion_event() {
+dataLayer.push({
+  event: "conversion_event",
+  lead_type: "test lead",
+  value: 5
+};
